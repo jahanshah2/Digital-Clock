e687c7ac6a1a70fb532fbb36f8448cf226fb5707
jahanshah2 <janalishah@gmail.com>
2024-04-26 08:39:50 +0500

    Solve AM or PM Error

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function clock() {
     if (h == 0) {
         h = 12;
     }
-    let ampm = h >= 12 ? "AM" : "PM";
+    let ampm = h <= 12 ? "AM" : "PM";
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
